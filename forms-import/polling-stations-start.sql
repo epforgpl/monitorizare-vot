f71cb5349f6ba91a20a26e39d80d9d368d4c7f6d
Krzysiek Madejski <krzysztof.madejski@epf.org.pl>
2018-10-28 13:04:48 +0100

    Delete all intermmediary data, move it to https://github.com/epforgpl/monitorizare-vot-deploy-poland2018

--- forms-import/polling-stations-start.sql
+++
@@ -1,6 +0,0 @@
-
--- \{"type": "district", "teryt_parent": (\d+), "number": "(\d+)", "place": "([^"]+)", "accessible": \w+\}
--- INSERT INTO PollingStations (Id, Address, IdCounty, TerritoryCode, Number) VALUES ($1 * 10000 + $2, '$3', (SELECT Id FROM Counties WHERE Code = '$1') , '$1', $2); SET @a = @a + 1;
-
-DECLARE @a int
-SET @a = 1
